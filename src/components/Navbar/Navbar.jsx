6ccbf7545fe94283c4239a85397a0d2c77c080bf
Nazar2015A <nazar.komar456@gmail.com>
2024-01-24 16:16:16 +0200

    Update Navbar and add logic for theGoal page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import foodMentorImg from "../../assets/img/foodMentorImg.png";
+import { useNavigate } from "react-router-dom";
+import left from '../../assets/img/left.svg'
 
 const Navbar = () => {
+  const navigate = useNavigate()
   return (
-    <div>Navbar</div>
-  )
-}
+    <StyledNavbar>
+      <StyledImg src={left} alt="Left Icon" onClick={() => navigate(-1)} />
+      <StyledMentorImg src={foodMentorImg} alt="Food Mentor Image" />
+      <StyledTitle>Food Mentor</StyledTitle>
+    </StyledNavbar>
+  );
+};
+const StyledNavbar = styled.header`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-export default Navbar
+const StyledTitle = styled.h2`
+  color: #374234;
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 22px;
+  letter-spacing: -0.025em;
+  margin-left: 10px;
+`;
+const StyledImg = styled.img`
+  cursor: pointer;
+  padding: 10px 12px;
+  border-radius: 5px;
+  margin-right: 18px;
+  transition: all 0.3s;
+
+  &:hover {
+    background: #eeeeee;
+  }
+
+`;
+const StyledMentorImg = styled.img``
+export default Navbar;
